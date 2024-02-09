a1fe669e56a80417981f6255f72f7c6df7c9500c
Shawn T. Amundson <amundson@bitwise.io>
2024-02-09 11:30:23 -0600

    Adhere to uninlined_format_args lint recommendation
    This doesn't currently trigger the lint, but it is fixed for a similar
    reason - avoiding unnecessary "{}", and in this case avoiding the
    write!() macro.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- errling/src/internal.rs
+++ errling/src/internal.rs
@@ -141,13 +141,13 @@ impl error::Error for InternalError {
 impl fmt::Display for InternalError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match &self.message {
-            Some(m) => write!(f, "{}", m),
+            Some(m) => f.write_str(m),
             None => match &self.source {
                 Some(s) => match &s.prefix {
                     Some(p) => write!(f, "{}: {}", p, s.source),
-                    None => write!(f, "{}", s.source),
+                    None => s.source.fmt(f),
                 },
-                None => write!(f, "{}", std::any::type_name::<InternalError>()),
+                None => f.write_str(std::any::type_name::<InternalError>()),
             },
         }
     }
